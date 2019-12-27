417236b8ddd469720195362e48f49bdf60f9c50d
s4na <appletea.umauma@gmail.com>
2019-12-28 05:50:50 +0900

    clean js file

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    for (let registration of registrations) {
+      registration.unregister();
+    }
+  });
+}
--- index.js
+++
@@ -1,7 +0,0 @@
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.getRegistrations().then(registrations => {
-    for (let registration of registrations) {
-      registration.unregister();
-    }
-  });
-}
